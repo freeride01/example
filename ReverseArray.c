e9ba8d67ec33ebedcd672a568682ec092b91a3c8
freeride01 <69974416+freeride01@users.noreply.github.com>
2021-04-02 14:07:14 +0300

    Add files via upload
    Reverse an integer array.

---
+++ ReverseArray.c
@@ -0,0 +1,39 @@
+//Reverse an array
+#include <stdio.h>
+
+#define SIZE 10
+
+int main(void){
+	
+	int array[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int reverseArray[SIZE];
+	int i, j;
+	
+	//Print array
+	puts("Array: ");
+	for(i = 0; i <= SIZE - 1; i++){
+		printf("%d ", array[i]);
+	}
+	
+	//Reverse the array
+	for(i = SIZE-1, j = 0; i >= 0, j <= SIZE-1; i--, j++){		
+		reverseArray[j] = array[i];
+		
+	}
+	
+	puts("\nReversed array: ");
+	for(j = 0; j <= SIZE - 1; j++){
+		printf("%d ", reverseArray[j]);
+	}
+	
+	//Shift to left
+	for (j = 0; j <= SIZE - 1; j++){
+		reverseArray[j] = reverseArray[j + 1];
+	}
+	
+	//Print shifted array
+	puts("\nLeft-shifted array: ");
+	for(j = 0; j <= SIZE - 1; j++){
+		printf("%d ", reverseArray[j]);
+	}
+}
